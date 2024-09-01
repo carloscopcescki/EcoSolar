00b8166c722694c6f1377e6275a5e894e2b337fa
Copcescki <carlos.copcescki@gmail.com>
2024-09-01 15:50:38 -0300

    Update dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,24 @@
-from commands import *
+import streamlit as st
 
-solar_dash = Dashboard()
-
-solar_dash.page()
-
+def main():
+    '''Create dashboard page'''
+    st.set_page_config(
+        page_title='Solar Panel',
+        page_icon=':sunrise:',
+        layout='wide'
+    )
+    # Sidebar elements
+    st.sidebar.empty()
+    st.sidebar.image('./img/fsa.png', use_column_width=True)
+    st.sidebar.title("FSA - Solar Dash")
+    st.sidebar.write("Dashboard desenvolvido para exibir quantidade necessária de painés solares na FSA + potencial energético da região.")
+        
+    # Main elements
+    st.title("Irradiação Solar Média - Grande ABC")
+    with open('mapa_interativo.html', 'r', encoding='utf-8') as f:
+        html_content = f.read()
+        
+    st.components.v1.html(html_content, width=700, height=500)
+    
+if __name__ == "__main__":
+    main()
