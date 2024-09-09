3fa9ffd94fb35da75fc5d7dad7a31df777dfc25e
Carlos Copcescki <41136340+carloscopcescki@users.noreply.github.com>
2024-09-09 15:32:03 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -12,14 +12,19 @@ def main():
     st.sidebar.image('./img/fsa.png', use_column_width=True)
     st.sidebar.title("FSA - Solar Dash")
     st.sidebar.write("Dashboard desenvolvido para exibir quantidade necessária de painés solares na FSA + potencial energético da região.")
-    
-    
+    st.sidebar.divider()
+
+    panel_potencial = st.sidebar.text_input("Potência do painel solar (em kW")
+    solar_irradiation = st.sidebar.text_input("Irradiação solar (em kWh/m².dia)")
+    system_efficiency = st.sidebar.text_input("Eficiência do sistema")
+    irradiation_days = st.sidebar.number_input("Número de dias", min_value=1, max_value=365, step=1)
+
     # Main elements
     st.title("Irradiação Solar Média - Grande ABC")
-    with open('irradiacao_media_abc.html', 'r', encoding='utf-8') as f:
+    with open('mapa_interativo.html', 'r', encoding='utf-8') as f:
         html_content = f.read()
         
-    st.components.v1.html(html_content, width=1200, height=650)
+    st.components.v1.html(html_content, width=750, height=600)
     
 if __name__ == "__main__":
     main()
