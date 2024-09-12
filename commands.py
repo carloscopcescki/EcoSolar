82da14437de51c47f9d2d1f4feed355cb7dd6f30
Carlos Copcescki <41136340+carloscopcescki@users.noreply.github.com>
2024-09-12 11:55:54 -0300

    Add files via upload

--- commands.py
+++ commands.py
@@ -1,3 +1,4 @@
+from lib2to3.pytree import convert
 import folium
 from streamlit_folium import st_folium
 from typing import Any
@@ -26,6 +27,47 @@ class Map:
         
         return st_data
     
-class Geolocation:
-    def __init__(self) -> None:
+class EnergyCalculate:
+    '''Class to calculate energy potential and solar panel potential'''
+    def __init__(self):
         pass
+
+    def energyGenerate(self, qty_panel: float, panel_potential: float, irradiation: float, efficiency: float, days: int) -> float | int:
+        '''Calculate energy generated'''
+        self.panel_potential = panel_potential
+        self.irradiation = irradiation
+        self.efficiency = efficiency
+        self.days = days
+        self.qty = qty_panel
+
+        media_efficiency = self.efficiency / 100
+        media_potential = self.panel_potential / 1000
+        energy = media_potential * self.irradiation * media_efficiency * self.days
+        energy_perpanel = energy * self.qty
+        energy_rounded = round(energy_perpanel, 2)
+
+        return energy_rounded
+
+class SolarSystem:
+    def __init__ (self, panel: float, consumption: float, irradiation: float, efficiency: float, days: int) -> float | int:
+        self.panel = panel
+        self.consumption = consumption
+        self.irradiation = irradiation
+        self.efficiency = efficiency
+        self.days = days
+        
+    def systemCapacity(self) -> float:
+        '''Return solar panel system capacity'''
+        cons_day = self.consumption / self.days
+        cons_irrad = cons_day / self.irradiation
+        media_efficiency = self.efficiency / 100
+        sys_capacity = cons_irrad / media_efficiency
+        self.media_capacity = round(sys_capacity, 2)
+        return self.media_capacity
+
+    def quantity(self) -> int:
+        '''Return solar panel quantity'''
+        convert_sys = self.media_capacity * 1000
+        qty = convert_sys / self.panel
+        qty_rounded = int(qty)
+        return qty_rounded
--- dashboard.py
+++ dashboard.py
@@ -10,27 +10,51 @@ def main() -> None:
         layout='wide'
     )
     
-    geolocator = Nominatim(user_agent="geoapiExercises")
+    geolocator = Nominatim(user_agent="geoapiExercises", timeout=None)
         
     # Sidebar elements
     st.sidebar.empty()
-    st.sidebar.title("FSA - Solar Dash")
+    st.sidebar.title("Solar Dash")
     st.sidebar.write("Dashboard desenvolvido para auxiliar na pesquisa e estudo acerca do potencial energético solar da FSA e a viabilidade de implementação dos painéis solares.")
-    st.sidebar.divider()
 
     # Input values
     st.sidebar.title("Inserir dados")
     search_location = st.sidebar.text_input("Buscar localização")
-    panel_potencial = st.sidebar.number_input("Potência do painel solar (em kW)", min_value=0)
-    solar_irradiation = st.sidebar.number_input("Irradiação solar (em kWh/m².dia)", min_value=0)
-    system_efficiency = st.sidebar.number_input("Eficiência do sistema (%)", min_value=0, max_value=100, step=5)
-    irradiation_days = st.sidebar.number_input("Número de dias", min_value=1, max_value=365, step=1)
+
+    with st.sidebar.expander("Energia gerada"):
+        st.info("Calcular a quantidade de energia gerada por painel solar")
+        panel_qty = st.slider("Quantidade de painéis", 0, 150, 1)
+        panel_potencial = st.number_input("Potência do painel solar (em kWh)", min_value=0)
+        solar_irrad_generate = st.number_input("Irradiação solar (em kWh/m².dia)", min_value=0.0)
+        sys_efficiency_generate = st.number_input("Eficiência do sistema (%)", key='efficiency-generated', min_value=0, max_value=100, step=5)
+        day_generate = st.number_input("Número de dias", key='days-generated', min_value=1, max_value=365, step=1)
+
+    with st.sidebar.expander("Quantidade de painéis solares"):
+        st.info("Obter a quantidade de painéis solares para um sistema fotovoltaico")
+        energy_consumption = st.number_input("Consumo médio mensal de energia (em kWh)", min_value=0)
+        panel = st.number_input("Potência do painel solar escolhido (em kWh)", min_value=0)
+        solar_irrad_panel = st.number_input("Irradiação solar (em kWh/m².dia)", key='irrad-panel', min_value=0.0)
+        sys_efficiency_panel = st.number_input("Eficiência do sistema (%)", key='efficiency-panel', min_value=0, max_value=100, step=5)
+        day_panel = st.number_input("Número de dias", key='days-panel', min_value=1, max_value=365, step=1)
     
+    bt_calc = st.sidebar.button("Calcular")
+
     # Main elements
     col1, col2 = st.columns(2)
     
     with col1:
         st.title("Dados")
+        if bt_calc:
+            calcular = EnergyCalculate()
+            response_energy = calcular.energyGenerate(panel_qty, panel_potencial, solar_irrad_generate, sys_efficiency_generate, day_generate)
+            st.subheader(f"Energia gerada: {response_energy}kWh")
+            
+            st.divider()
+
+            panel = SolarSystem(panel, energy_consumption, solar_irrad_panel, sys_efficiency_panel, day_panel)
+            st.subheader(f"Capacidade do sistema: {panel.systemCapacity()}kW")
+            st.subheader(f"Quantidade necessária: {panel.quantity()} painéis solares.")
+
     with col2:
         st.title("Mapa")
         if search_location == "":
