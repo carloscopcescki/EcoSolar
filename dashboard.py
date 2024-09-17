d39a543cd8e725497940bc40b0f285cd36c9f208
Copcescki <carlos.copcescki@gmail.com>
2024-09-17 00:22:41 -0300

    Update values

--- dashboard.py
+++ dashboard.py
@@ -27,27 +27,27 @@ def main() -> None:
         
     # Sidebar elements
     st.sidebar.empty()
-    st.sidebar.image('./img/fsa.png', width=150)
+    st.sidebar.image('./img/fsa.png', width=250)
     st.sidebar.title("Solar Dash")
     st.sidebar.write("Dashboard desenvolvido para auxiliar na pesquisa e estudo acerca do potencial energético solar da FSA e a viabilidade de implementação dos painéis solares.")
-
+    
     # Input values
     st.sidebar.title("Inserir dados")
     search_location = st.sidebar.text_input("Pesquise um endereço", placeholder="Insira uma localização", value="Fundação Santo André")
 
-    with st.sidebar.expander("Energia gerada"):
+    with st.sidebar.expander("Energia gerada", expanded=True):
         st.info("Calcular a quantidade de energia gerada por painel solar")
         panel_qty = st.slider("Painéis solares", 0, 150, 1)
-        panel_potencial = st.number_input("Potência do painel solar (em kWh)", min_value=0)
-        solar_irrad_generate = st.number_input("Irradiação solar (em kWh/m².dia)", min_value=0.0, value=4.00)
+        panel_potencial = st.number_input("Potência do painel solar (em kWh)", min_value=0, value=400)
+        solar_irrad_generate = st.number_input("Irradiação solar (em kWh/m².dia)", min_value=0.0, value=4.53)
         sys_efficiency_generate = st.number_input("Eficiência do sistema (%)", key='efficiency-generated', min_value=0, max_value=100, step=5, value=80)
         day_generate = st.number_input("Número de dias", key='days-generated', min_value=1, max_value=365, step=1, value=30)
 
-    with st.sidebar.expander("Quantidade de painéis solares"):
+    with st.sidebar.expander("Quantidade de painéis solares", expanded=True):
         st.info("Calcular a quantidade de painéis solares para o sistema")
         energy_consumption = st.number_input("Consumo médio mensal de energia (em kWh)", min_value=0, value=550)
         panel = st.number_input("Potência do painel solar (em kWh)", key="panel_potential", min_value=0, value=400)
-        solar_irrad_panel = st.number_input("Irradiação solar (em kWh/m².dia)", key='irrad-panel', min_value=0.0, value=4.00)
+        solar_irrad_panel = st.number_input("Irradiação solar (em kWh/m².dia)", key='irrad-panel', min_value=0.0, value=4.53)
         sys_efficiency_panel = st.number_input("Eficiência do sistema (%)", key='efficiency-panel', min_value=0, max_value=100, step=5, value=80)
         day_panel = st.number_input("Número de dias", key='days-panel', min_value=1, max_value=365, step=1, value=30)
     
@@ -65,15 +65,15 @@ def main() -> None:
 
         col1a, col1b = st.columns(2)
         with col1a:
-            st.image('./img/tomada.jpg', width=150)
+            st.image('./img/tomada.jpg', width=170)
             st.text(f"Energia gerada: {calculate.generate(panel_qty, panel_potencial, solar_irrad_generate, sys_efficiency_generate, int(day_generate))}kWh")
         with col1b:
-            st.image('./img/system.jpg', width=150)
+            st.image('./img/system.jpg', width=170)
             st.text(f"Capacidade do sistema: {panel_sys.capacity(panel, energy_consumption, solar_irrad_panel, sys_efficiency_panel, int(day_panel))}kW")
         
         col2a, col2b = st.columns(2)
         with col2a:
-            st.image('./img/solar_panel.png', width=150)
+            st.image('./img/solar_panel.png', width=170)
             st.text(f"Quantidade de painéis: {panel_sys.quantity()}")
 
     with col2:
