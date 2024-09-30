858f3fe871492a4dee7190222934099377a84ed0
Carlos Copcescki <41136340+carloscopcescki@users.noreply.github.com>
2024-09-30 11:15:43 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -87,8 +87,8 @@ def main() -> None:
                 map_location.map_generate()
     
     with col2a:
-        st.image("./img/brasil_dni.png", caption="Solar resource map © 2021 Solargis")
-        st.image("./img/brasil_pvout.png", caption="Solar resource map © 2021 Solargis")
+        st.image("./img/brasil_dni.png", caption="Solar resource map © 2021 Solargis", width=300)
+        st.image("./img/brasil_pvout.png", caption="Solar resource map © 2021 Solargis", width=300)
     
     st.divider()
 
