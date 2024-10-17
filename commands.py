5956fdb6e078a7024074c940245a4540670c35e3
Carlos Copcescki <41136340+carloscopcescki@users.noreply.github.com>
2024-10-17 09:44:34 -0300

    Update commands.py

--- commands.py
+++ commands.py
@@ -98,14 +98,12 @@ class EnergyCalculate:
         self.longitude = longitude
         self.azimuth = azimuth
         self.tilt = tilt
-        
-        locale.setlocale(locale.LC_TIME, 'pt_BR')
-        
+    
         site = Location(latitude, longitude, tz='America/Sao_Paulo')
         times = pd.date_range('2024-01-01', '2024-12-31', freq='H', tz=site.tz)
         solar_position = site.get_solarposition(times)
         irradiation_data = site.get_clearsky(times)
-        
+    
         poa_irrad = irradiance.get_total_irradiance(
             surface_tilt=self.tilt,
             surface_azimuth=self.azimuth,
@@ -115,12 +113,20 @@ class EnergyCalculate:
             ghi=irradiation_data['ghi'],
             dhi=irradiation_data['dhi']
         )
-        
+    
         hourly_production = (poa_irrad['poa_global'] / 1000) * (self.panel_potential / 100) * self.efficiency
         daily_production = hourly_production.resample('D').sum()
         monthly_production = daily_production.resample('M').sum()
         monthly_production.index = monthly_production.index.strftime('%B')
-        
+    
+        meses_traduzidos = {
+            'January': 'Janeiro', 'February': 'Fevereiro', 'March': 'Março', 'April': 'Abril',
+            'May': 'Maio', 'June': 'Junho', 'July': 'Julho', 'August': 'Agosto',
+            'September': 'Setembro', 'October': 'Outubro', 'November': 'Novembro', 'December': 'Dezembro'
+        }
+    
+        monthly_production.index = monthly_production.index.strftime('%B').map(meses_traduzidos)
+    
         fig = go.Figure()
         fig.add_trace(go.Bar(
             x=monthly_production.index,
@@ -130,9 +136,9 @@ class EnergyCalculate:
         ))
 
         fig.update_layout(
-            title_text='Produção de Energia Solar (por mês)',
+            title_text='Produção de Energia Solar',
             xaxis=dict(title='Mês', type='category'),
             yaxis=dict(title='Energia (kWh)')
         )
-        
+    
         return st.plotly_chart(fig)
